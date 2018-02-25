8cce7c37c40d6fcb9a4ef8cefdb31388d7fa3de8
Marcelo <mvrozanti@hotmail.com>
2018-02-25 19:38:55 -0300

    Sun Feb 25 19:38:55 -03 2018

--- grab_updated_and_push.sh
+++ grab_updated_and_push.sh
@@ -46,6 +46,7 @@ cp -fR /usr/local/bin/regextract usr-local-bin
 cp -fR /usr/local/bin/usage usr-local-bin
 cp -fR /usr/local/bin/todo usr-local-bin
 cp -fR /usr/local/bin/mpc-play-pause usr-local-bin
+cp -fR /usr/local/bin/pipes usr-local-bin
 
 # push
 gitap "$(date)"
