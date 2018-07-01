0ac7c3f53d1ee2ebf79ffc3f2c0600025648473c
Nexor <mvrozanti@hotmail.com>
2018-07-01 19:33:17 -0300

    mbtc-toggle added

--- usr-local-bin/mbtc-remove.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xdotool mousemove --sync 2905 1069 click 3 sleep 0.2 mousemove_relative --sync 0 -45 sleep 0.2 click --delay 100 1 mousemove_relative --sync 0 -1 mousemove_relative --sync 0 -1 mousemove_relative --sync 0 -1 sleep 0.1 click --delay 50 1 sleep 0.1 click 1 mousedown 1 sleep 0.1 mouseup 1
---
+++ usr-local-bin/mbtc-toggle.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xdotool mousemove --sync 2803 1069 click 3 sleep 0.1 key Down space sleep 0.7 key Tab key Tab key space key "Super_L+q"
